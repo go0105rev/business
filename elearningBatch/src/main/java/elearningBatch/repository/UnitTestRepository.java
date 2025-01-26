f11332b9c1266f03aba1b16744afc88beac40673
go <go@revale.co.jp>
2025-01-27 01:12:49 +0900

    unitTestのrepository修正

--- elearningBatch/src/main/java/elearningBatch/repository/UnitTestRepository.java
+++ elearningBatch/src/main/java/elearningBatch/repository/UnitTestRepository.java
@@ -2,11 +2,20 @@ package elearningBatch.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
 import elearningBatch.model.UnitTest;
 
+@Repository
 public interface UnitTestRepository {
 
     List<UnitTest> findAll();
+
+    List<UnitTest> findAsVersion0();
     
-    long updateAsQues();
+    long countAsQuesNum(@Param("quesNum") String quesNum, @Param("userId") String userId);
+
+    void updateAsQues(@Param("entity") UnitTest entity,
+            @Param("sourceId") String sourceId);
 }
--- elearningBatch/src/main/java/elearningBatch/service/evaculation/codeTestServiceImpl.java
+++ elearningBatch/src/main/java/elearningBatch/service/evaculation/codeTestServiceImpl.java
@@ -2,29 +2,53 @@ package elearningBatch.service.evaculation;
 
 import java.util.List;
 
+import jakarta.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import elearningBatch.model.UnitTest;
+import elearningBatch.repository.UnitTestRepository;
 import elearningBatch.service.Tasklets;
 
 @Service
 public class codeTestServiceImpl extends Tasklets {
 
     Logger logger = LoggerFactory.getLogger(codeTestServiceImpl.class);
-    
+
+    @Inject
+    UnitTestRepository repository;
+
     @Override
     public Status process(List<String> input) {
         logger.debug("---------------start---------------");
-        /*DB select*/
 
-        /*target java create*/
+        List<UnitTest> targets = repository.findAsVersion0();
 
-        /*java test*/
+        targets.forEach(t -> {
 
-        /*result db update*/
+            long a = repository.countAsQuesNum(t.getQuesNum(), t.getUserId());
+            if (a > 1) {
+                logger.error(
+                        "[ONLY CAN HAVE ONE OF SINGLE QUESNUM SINGLE USERID]: "+t.getSourceId());
+                // TODO 終了せず、次の要素移行するか要確認
+                return;
+            }
+
+            /* target java create */
+
+            /* java test */
+
+            UnitTest e = new UnitTest();
+            e.setScore(Long.valueOf(80));
+            e.setVersion((short) 1);
+            repository.updateAsQues(e, t.getSourceId());
+
+        });
 
         logger.debug("---------------end---------------");
+
         return Status.OK;
     }
 
